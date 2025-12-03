fbb2c6033a7bede338051e67dfa3ec35d5209a0f
psc3245 <psc3.omaha@gmail.com>
2025-12-03 11:57:37 -0600

    workin

--- coms352/p2/journal.c
+++ coms352/p2/journal.c
@@ -9,11 +9,56 @@ int is_journal_txe_complete;
 int is_write_bitmap_complete;
 int is_write_inode_complete;
 
+typedef struct {
+    int buffer[BUFFER_SIZE];
+    int in;
+    int out;
+    int count;
+    pthread_mutex_t mutex;
+    sem_t empty_slots;
+    sem_t full_slots;
+} circular_buffer_t;
+
+circular_buffer_t request_buffer;
+circular_buffer_t journal_metadata_buffer;
+circular_buffer_t journal_commit_buffer;
+
+static pthread_t journal_metadata_write_thread; 
+static pthread_t journal_commit_write_thread;
+static pthread_t checkpoint_metadata_thread;
+
+sem_t journal_metadata_wait;
+sem_t journal_commit_wait;
+sem_t checkpoint_wait;
+
+void *journal_metadata_write_worker(void *arg);
+void *journal_commit_write_worker(void *arg);
+void *checkpoint_metadata_worker(void *arg);
+
+
+
 /* This function can be used to initialize the buffers and threads.
  */
 void init_journal() {
-	// initialize buffers and threads here
+    // Initialize your semaphores/mutexes here first!
+    // ...
+
+        // Create Thread 1
+        if (pthread_create(&journal_metadata_write_thread, NULL, journal_metadata_write_worker, NULL) != 0) {
+                perror("Failed to create journal_metadata_write_thread");
+        }
+
+        // Create Thread 2
+        if (pthread_create(&journal_commit_write_thread, NULL, journal_commit_write_worker, NULL) != 0) {
+                perror("Failed to create journal_commit_write_thread");
+        }
+
+        // Create Thread 3
+        if (pthread_create(&checkpoint_metadata_thread, NULL, checkpoint_metadata_worker, NULL) != 0) {
+                perror("Failed to create checkpoint_metadata_thread");
+        }
 }
+
 
 /* This function is called by the file system to request writing data to
  * persistent storage.
@@ -79,6 +124,24 @@ void request_write(int write_id) {
         // tell the file system that the write is complete
         write_complete(write_id);
 }
+
+// Function YOU must write
+void *journal_metadata_write_worker(void *arg) {
+    while(1) { // <--- The infinite loop required by the PDF
+        // 1. Wait for "buffer not empty"
+        // 2. Take write_id out
+        // 3. Do the work (call issue_... functions)
+    }
+}
+
+void *journal_commit_write_worker(void *arg) {
+
+}
+
+void *checkpoint_metadata_worker(void *arg) {
+        
+}
+
 
 /* This function is called by the block service when writing the txb block
  * to persistent storage is complete (e.g., it is physically written to  
